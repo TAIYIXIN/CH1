4e5a5439e8dcec8b3f705b48464b065202856031
memorylucky <nelson318004@gmail.com>
2017-09-26 15:48:43 +0800

    ch0

---
+++ Ch1_Lab1/source/Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("Welcome to C!\n");
+	printf("This is Ch0-Lab4, my name is Nelson\n");
+	printf("My student id is 106360223\n");
+
+	printf("My name is Nelson\n");
+
+	system("pause");
+		return 0;
+
+}
